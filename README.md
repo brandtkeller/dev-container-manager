8d149ae6857c4c43b936610522ab74126811cbc2
Brandt Keller <brandt.keller@defenseunicorns.com>
2024-07-05 18:32:01 +0000

    feat(viper): structure for viper variable use

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# dev-container-manager
-manage local ephemeral development environments with dcm
+# Dev Container Manager (dcm)
+
+dcm makes ephemeral development environments easy. 
+
+## How does it work?
+
+Given a docker compose file, dcm will manage the start -> stop -> rebuild -> start lifecycle of your development containers.
+
+This is managed by a unix-cron string format to perform the rebuild on your desired schedule.
+
+Annotations in your docker compose file will be used to determine whether a service is ephemeral or persistent
+
+**Ephemeral** - All services are assumed to be ephemeral - meaning that `dcm` will execute on the schedule of your choosing and perform either a pull or build of your development containers.
+
+**Persistent** - Services with the `dcm: persistent` annotation will be started and managed by `dcm` but will not be rebuilt or modified on the established schedule.
+
+## Why?
+Use dcm to control ensuring you always have the latest packages installed in your development containers. It also is a forcing-function to ephemeral development - a best practice in current development whereby the only tooling that exists at the start are those declared in your image build process.
+
+
+## TODO:
+- Write a command and functionality to mimic cron
+- Consume a docker compose file and determine the services to be managed
+- Determine access to annotations
+- Determine helper commands for manual intervention
--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -9,10 +8,12 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
 
 var cfgFile string
+var test string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -26,7 +27,15 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+
+		if test != "" {
+			fmt.Printf("test: %s\n", test)
+		} else {
+			fmt.Println("No test variable configured")
+		}
+
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -47,31 +56,57 @@ func init() {
 
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.dev-container-manager.yaml)")
 
+	rootCmd.Flags().StringVarP(&test, "test", "t", "", "a test message")
+
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
+	v := viper.New()
+
 	if cfgFile != "" {
 		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
+		v.SetConfigFile(cfgFile)
 	} else {
 		// Find home directory.
 		home, err := os.UserHomeDir()
 		cobra.CheckErr(err)
 
 		// Search config in home directory with name ".dev-container-manager" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName(".dev-container-manager")
+		v.AddConfigPath(home)
+		v.SetConfigType("yaml")
+		v.SetConfigName(".dev-container-manager")
 	}
 
-	viper.AutomaticEnv() // read in environment variables that match
+	v.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	if err := v.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", v.ConfigFileUsed())
 	}
+
+	bindFlags(rootCmd, v)
+}
+
+// Bind each cobra flag to its associated viper configuration (config file and environment variable)
+func bindFlags(cmd *cobra.Command, v *viper.Viper) {
+	cmd.Flags().VisitAll(func(f *pflag.Flag) {
+		// Determine the naming convention of the flags when represented in the config file
+		configName := f.Name
+		// If using camelCase in the config file, replace hyphens with a camelCased string.
+		// Since viper does case-insensitive comparisons, we don't need to bother fixing the case, and only need to remove the hyphens.
+		// if replaceHyphenWithCamelCase {
+		// 	configName = strings.ReplaceAll(f.Name, "-", "")
+		// }
+		fmt.Printf("configName: %s / changed: %v / set: %v\n", configName, f.Changed, v.IsSet(configName))
+
+		// Apply the viper config value to the flag when the flag is not set and viper has a value
+		if !f.Changed && v.IsSet(configName) {
+			val := v.Get(configName)
+			cmd.Flags().Set(f.Name, fmt.Sprintf("%v", val))
+		}
+	})
 }
